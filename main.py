0b716505ff108125a5856b73c8d440aae68c1bf9
smyk07 <samyak.bambole07@gmail.com>
2024-09-04 10:49:34 +0530

    change commands to isciie and isciid for encoding and decoding respectively [main]

--- main.py
+++ main.py
@@ -22,29 +22,29 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if (
-        message.content.startswith("iscii ")
-        or message.content.startswith("Iscii ")
-    ):
-        if len(message.content) > 6:
-            args = message.content[6:]
+    if message.content.startswith("isciie") or message.content.startswith("Isciie"):
+        if len(message.content) > 7:
+            args = message.content[7:]
             await message.reply(iscii.encode(args))
+        elif message.reference:
+            replied_message = await message.channel.fetch_message(
+                message.reference.message_id
+            )
+            await message.reply(iscii.encode(replied_message.content))
         else:
             await message.add_reaction("😹")
 
-    if (
-        message.content.startswith("isciidecode")
-        or message.content.startswith("Isciidecode")
-    ): 
-        if len(message.content) > 12: 
-            args = message.content[12:]
+    if message.content.startswith("isciid") or message.content.startswith("Isciid"):
+        if len(message.content) > 7:
+            args = message.content[7:]
             await message.reply(iscii.decode(args))
-        elif message.reference: 
-            replied_message = await message.channel.fetch_message(message.reference.message_id)
+        elif message.reference:
+            replied_message = await message.channel.fetch_message(
+                message.reference.message_id
+            )
             await message.reply(iscii.decode(replied_message.content))
         else:
             await message.add_reaction("😹")
-
 
     if message.content == "!cabbit":
         await message.channel.send(
